70d108b61ec21a47219221018c6dc763dd916234
stefanodvx <pp.hartix@gmail.com>
2022-03-22 18:25:36 +0100

    Update utils.py

--- flixcrack/utils.py
+++ flixcrack/utils.py
@@ -90,7 +90,6 @@ supported_audio_profiles = {
 
 def get_profiles(video_profile: str, audio_profile: str, quality: int):
     profiles = [
-        "dfxp-ls-sdh",
         "webvtt-lssdh-ios8",
         "BIF240",
         "BIF320"

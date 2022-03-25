013da7e46673c7f3996ed4064834be5c642b1acf
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2022-03-25 14:26:31 +0100

    Update converter.py

--- flixcrack/converter.py
+++ flixcrack/converter.py
@@ -1,57 +1,72 @@
 import re
 
-# Code from Netflix-To-Srt
-# https://github.com/isaacbernat/netflix-to-srt
+timing_regex = re.compile(
+    r"([\d\.:]+ \-\-\> [\d\.:]+) " + \
+    r"position:[\d.\.]+%,\w+ align:(\w+) size:[\d.\.]+% line:([\d.\.]+)"
+)
 
-def convert_vtt_time(line):
-    times = line.replace(".", ",").split(" --> ")
-    if len(times[0]) == 9:
-        times = ["00:" + t for t in times]
-    return "{} --> {}".format(times[0], times[1].split(" ")[0])
+def fix(text: str):
+    fixes = [
+        ["\r", ""],
+        ["\n", ""],
+        ["&lt;", "<"],
+        ["&gt;", ">"],
+        ["&lrm;", "\u200e"],
+        ["&rlm;", "\u200f"],
+        ["&nbsp;", "\u00a0"],
+        ["&amp;", "&"]
+    ]
+    for sf, r in fixes:
+        text = text.replace(sf, r)
+    text = re.sub("<\/?(([^/ibu>]{1})|([^/>]{2,}?))>", "", text)
+    return text
 
-def vtt_to_srt(text):
-    if not text.startswith(u"\ufeffWEBVTT") and not text.startswith(u"WEBVTT"):
-        raise Exception(".vtt format must start with WEBVTT, wrong file?")
-    styles = get_vtt_styles(text)
-    style_tag_re = re.compile(u'<c\.(.*)>(.*)</c>')
+class Converter:
+    def __init__(self, file: str):
+        self.name = file
+        self.raw_name = ".".join(file.split(".")[:-1])
 
-    lines = []
-    current_sub_line = []
-    for line in text.split("\n"):
-        if current_sub_line:
-            if line:
-                style_tag = re.search(style_tag_re, line)
-                if style_tag:
-                    line = style_tag.group(2) 
-                    color = styles.get(style_tag.group(1).split(".")[0])
-                    if color:
-                        line = u"<font color={}>{}</font>".format(
-                            color, line)
-                current_sub_line.append(line)
+    def to_srt(self):
+        new_lines = []
+        timing_regex = re.compile(
+            " ".join([
+                r"([\d\.:]+ \-\-\> [\d\.:]+)",
+                r"position:[\d.\.]+%,\w+ align:(\w+) size:[\d.\.]+% line:([\d.\.]+)"
+            ])
+        )
+        lines = open(self.name, "r", encoding="utf8").readlines()
+        
+        verical_pos = -1     
+        line_pos_matrix = [
+            ["7", "8", "9"],
+            ["4", "5", "6"],
+            ["1", "2", "3"]
+        ]
+        
+        for i, line in enumerate(lines):
+            line = line.replace("\r", "")
+            if line == "\n" or line.startswith("NOTE"):
+                continue
+            match = timing_regex.search(line)
+            if not match:
+                continue
+            time = match.group(1).replace(".", ",")
+            align_type = match.group(2)
+            line_value = float(match.group(3))
+            if line_value <= 25.00:
+                verical_pos = 0
+            elif line_value >= 75.00:
+                verical_pos = 2
             else:
-                lines.append("\n".join(current_sub_line) + "\n\n")
-                current_sub_line = []
-
-        elif " --> " in line:
-            current_sub_line = [convert_vtt_time(line)]
-    if current_sub_line:
-        lines.append("\n".join(current_sub_line))
-
-    return "".join((u"{}\n{}".format(i, l) for i, l in enumerate(lines, 1)))
-
-
-def get_vtt_styles(text):
-    styles = {}
-    lines = text.split("\n")
-    n = 0
-    style_name_re = re.compile(u'::cue\(\.(.*)\).*')
-    color_re = re.compile(u'.*color: (\#.*);')
-    while n < len(lines):
-        style_name = re.search(style_name_re, lines[n])
-        if style_name and style_name.groups():
-            name = style_name.group(1)
-            color = re.search(color_re, lines[n + 1])
-            if color and color.groups():
-                styles[name] = color.group(1)
-        n += 1
-    return styles
+                verical_pos = 1
+            horizontal_pos = {
+                "middle": 1,
+                "center": 1,
+            }.get(align_type, -1)
+            position = line_pos_matrix[verical_pos][horizontal_pos]
+            new_lines.append(
+                f"{time}\n" + ("{\\an%s}" % position) \
+                .replace("{\\an2}", "") + fix(lines[i+1]) + "\n\n"
+            )
+        with open(self.raw_name + ".srt", "w+", encoding="utf-8") as f:
+            f.writelines(new_lines)

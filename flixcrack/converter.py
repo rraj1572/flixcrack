d246dca6e6a888d5ff3fba457b8db54dba210000
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2022-03-28 15:01:12 +0200

    Update converter.py

--- flixcrack/converter.py
+++ flixcrack/converter.py
@@ -1,10 +1,5 @@
 import re
 
-timing_regex = re.compile(
-    r"([\d\.:]+ \-\-\> [\d\.:]+) " + \
-    r"position:[\d.\.]+%,\w+ align:(\w+) size:[\d.\.]+% line:([\d.\.]+)"
-)
-
 def fix(text: str):
     fixes = [
         ["\r", ""],
@@ -28,15 +23,17 @@ class Converter:
 
     def to_srt(self):
         new_lines = []
-        timing_regex = re.compile(
+        subtitle_esp = re.compile(
             " ".join([
                 r"([\d\.:]+ \-\-\> [\d\.:]+)",
-                r"position:[\d.\.]+%,\w+ align:(\w+) size:[\d.\.]+% line:([\d.\.]+)"
-            ])
+                r"position:[\d.\.]+%,\w+ ",
+                r"align:(\w+) ",
+                r"size:[\d.\.]+% ",
+                r"line:([\d.\.]+)\n(.+)"
+            ]), re.DOTALL | re.MULTILINE
         )
-        lines = open(self.name, "r", encoding="utf8").readlines()
+        lines = open(self.name, "r", encoding="utf8").read().split("\n\n")
         
-        line_index = 1
         verical_pos = -1     
         line_pos_matrix = [
             ["7", "8", "9"],
@@ -44,16 +41,11 @@ class Converter:
             ["1", "2", "3"]
         ]
         
-        for i, line in enumerate(lines):
-            line = line.replace("\r", "")
-            if line == "\n" or line.startswith("NOTE"):
-                continue
-            match = timing_regex.search(line)
-            if not match:
-                continue
-            time = match.group(1).replace(".", ",")
-            align_type = match.group(2)
-            line_value = float(match.group(3))
+        for i, subtitle in enumerate(subtitle_esp.findall(lines), 1):
+            time = subtitle[0].replace(".", ",")
+            align_type = subtitle[1]
+            line_value = float(subtitle[2])
+            text = subtitle[3].strip()
             if line_value <= 25.00:
                 verical_pos = 0
             elif line_value >= 75.00:
@@ -66,9 +58,8 @@ class Converter:
             }.get(align_type, -1)
             position = line_pos_matrix[verical_pos][horizontal_pos]
             new_lines.append(
-                f"{line_index}\n" + f"{time}\n" + ("{\\an%s}" % position) \
-                .replace("{\\an2}", "") + fix(lines[i+1]) + "\n\n"
+                f"{i}\n" + f"{time}\n" + ("{\\an%s}" % position) \
+                .replace("{\\an2}", "") + fix(text) + "\n\n"
             )
-            line_index += 1
         with open(self.raw_name + ".srt", "w+", encoding="utf-8") as f:
             f.writelines(new_lines)

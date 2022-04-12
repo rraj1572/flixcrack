f39065b1e586e7c8a038d7856b75f9e04a213c30
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2022-04-13 01:10:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="flixcrack",
-    version="2022.4.8",
+    version="2022.4.13",
     author="stefanodvx",
     author_email="pp.stefanodvx@gmail.com",
     description="Netflix API Metadata & Downloader for Windows and Linux",

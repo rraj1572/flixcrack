13096e09dde5c6075907756f570f231d976dc5c9
SKR <64830000+skr1405@users.noreply.github.com>
2022-04-16 16:33:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "requests",
         "protobuf",
         "pycryptodomex",
-        "git+https://github.com/tpubg583/hyper@development" if sys.verison_info.major==3 and sys.version_info.minor>=10 else "hyper"
+        "git+https://github.com/tpubg583/hyper@development" if sys.version_info.major==3 and sys.version_info.minor>=10 else "hyper"
     ],
     packages=setuptools.find_packages(),
     python_requires=">=3.9.8",

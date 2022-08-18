438bb511e5b47b79d7f3152b4afb739def0aa69a
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2022-08-18 14:22:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setuptools.setup(
         "hyper @ git+https://github.com/tpubg583/hyper@development" if sys.version_info.major==3 and sys.version_info.minor>=10 else "hyper"
     ],
     packages=setuptools.find_packages(),
-    python_requires=">=3.9.8",
+    python_requires=">=3.7.4",
 )
